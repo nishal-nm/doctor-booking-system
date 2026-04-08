baf1d96693c30e4c6995fa379c13e961496d283c
Nishal NM <nishalshalu17@gmail.com>
2026-04-08 20:08:32 +0530

    env configured

--- doctor_booking/settings.py
+++ doctor_booking/settings.py
@@ -19,11 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-gbv18m0e(3@iv+uz&-k-+(iz031m#qm*441mdl6e%i9wcx-r$h'
+from decouple import config
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
